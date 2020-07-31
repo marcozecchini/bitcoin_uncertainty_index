acf8f1a22e4bafaa378891d608f4c5143e30b3f2
Marco Zecchini <zecchini@diag.uniroma1.it>
2020-07-31 11:09:53 +0200

    first version

--- make_historicals.py
+++ make_historicals.py
@@ -15,11 +15,12 @@ with open("twitter_credentials.json", "r") as file:
 python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 
 # Create our query
-query = {'q': 'bitcoin',
+query = {'q': 'algorand',
         'lang': 'en',
          'result_type': 'relevant',
          'count': '100',
         }
+currency = 'ALGO'
 
 # iteration index
 index = 0
@@ -28,10 +29,11 @@ index = 0
 currency_code = 'EUR'  # can also use EUR, CAD, etc.
 
 # time interval window
-WAIT_WINDOW = 1
+WAIT_WINDOW_IN_WINDOW = 20
 
+DELETE_WINDOW_IN_SECONDS = 86400 # ONE day
 
-def retrieve_and_save(index):
+def retrieve_and_save(index,df):
     # Search tweets
     dict_ = {'user': [], 'date': [], 'text': []}
     for status in python_tweets.search(**query)['statuses']:
@@ -40,17 +42,30 @@ def retrieve_and_save(index):
         dict_['text'].append(status['text'])
 
     # Make the request
-    price = client.rates = client.get_exchange_rates(currency='BTC',)
+    price = client.rates = client.get_exchange_rates(currency=currency,)
 
-    print ('Current bitcoin price in %s: %s' % (currency_code, price.rates.EUR)) ## Capisci come calcolare percentage
+    print ('Current %s price in %s: %s' % (currency, currency_code, price.rates.EUR)) ## Capisci come calcolare percentage
 
     # Structure data in a pandas DataFrame for easier manipulation
-    df = pd.DataFrame(dict_)
-    df.sort_values(by='date', inplace=True, ascending=True)
-    df['index'] = [index for _ in range(0, len(df))]
-    df['price'] = [price.rates.EUR for _ in range(0, len(df))]
+    # df = pd.read_csv("my_result.csv")
+    temp = pd.DataFrame(dict_)
+    temp.sort_values(by='date', inplace=True, ascending=True)
+    temp['index'] = [index for _ in range(0, len(temp))]
+    temp['price'] = [price.rates.EUR for _ in range(0, len(temp))]
 
-    df.to_csv('my_result.csv', mode='a', header=False, columns=['index', 'user','date','text', 'price'])
+    df = pd.concat([df, temp])
+    df.drop_duplicates(subset=['text','date'], inplace=True) ## delete duplicate
+
+    # write ONLY new ones
+    res = df[df['index'] == index]
+    res.to_csv('my_result.csv', mode='a', header=False, columns=['index', 'user','date','text', 'price'])
+
+    # drop old tweets - posso anche usare timespan
+    Dindex = DELETE_WINDOW_IN_SECONDS / 60*WAIT_WINDOW_IN_WINDOW
+    old_index = index - Dindex
+    df = df[df['index'] >= old_index ]
+    return df
+    
 
 if __name__ == "__main__":
     # Search tweets
@@ -61,9 +76,9 @@ if __name__ == "__main__":
         dict_['text'].append(status['text'])
 
     # Make the request
-    price = client.rates = client.get_exchange_rates(currency='BTC',)
+    price = client.rates = client.get_exchange_rates(currency=currency,)
 
-    print ('Current bitcoin price in %s: %s' % (currency_code, price.rates.EUR)) ## Capisci come calcolare percentage
+    print ('Current %s price in %s: %s' % (currency, currency_code, price.rates.EUR)) ## Capisci come calcolare percentage
 
     # Structure data in a pandas DataFrame for easier manipulation
     df = pd.DataFrame(dict_)
@@ -73,6 +88,6 @@ if __name__ == "__main__":
     df.to_csv('my_result.csv', columns=['index', 'user','date','text', 'price'])
 
     while True:
-        time.sleep(WAIT_WINDOW * 60)
+        time.sleep(WAIT_WINDOW_IN_WINDOW * 60)
         index += 1
-        retrieve_and_save(index)
+        df = retrieve_and_save(index, df)
